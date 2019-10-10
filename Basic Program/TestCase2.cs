52f311a1a85c3b9fbb00c386b33c043ed9d0eadc
Zafar Ahmad <ZAhmad@pattersoncompanies.com>
2019-10-10 16:17:03 +0500

    Change Local2

---
+++ Basic Program/TestCase2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Program
+{
+    class TestCase2
+    {
+        public void Signout(String Btn, String Test)
+        {
+            Btn = "Login";
+            Test = "Hello";
+        }
+}
